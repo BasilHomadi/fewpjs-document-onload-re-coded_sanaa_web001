fdc7daf704b0708b67569d3cbdff75b7802afc89
Basil Homadi <deviltearbasil@gmail.com>
2020-09-10 12:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function(){
   // console.log("The DOM has loaded
   let text = document.getElementById("text")
-  text.textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
+  text.textContent = "This is really cool!"
 })
 
 console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered")
